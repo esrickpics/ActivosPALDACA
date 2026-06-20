d70414272967910dbcaa26139514d63950b3c396
Ricardo Goitia <ricardogoitia108@gmail.com>
2026-06-20 17:07:00 -0400

    Update db.py

--- SSAPI/db.py
+++ SSAPI/db.py
@@ -1,3 +1,8 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASEDES = DATABASESDESARROLLO = {
     'default': {
@@ -12,27 +17,11 @@ DATABASEDES = DATABASESDESARROLLO = {
 
 DATABASEPROD = DATABASESPRODUCCION = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ssapmcco_PALDACADB',
-        'USER': 'ssapmcco_admin',
-        'PASSWORD': 'AdminPaldaca',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-MYSQL= {
-    'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('MYSQL_DB', 'paldaca_db'),
-        'USER': os.getenv('MYSQL_USER', 'RAG'),
-        'PASSWORD': os.getenv('MYSQL_PASSWORD', '12345'),
-        'HOST': os.getenv('MYSQL_HOST', 'localhost'),
-        'PORT': os.getenv('MYSQL_PORT', '3306'),
+        'NAME': 'ssapmcco_PALDACA_DB',
+        'USER': 'ssapmcco_ADMIN',
+        'PASSWORD': 'ADMINPALDACA12345',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
